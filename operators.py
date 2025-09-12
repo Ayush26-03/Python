240d5cedd0189c03b6b4220fd668b5993282ccb1
Ayush26-03 <ayushp4062@gmail.com>
2025-09-12 22:57:04 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,77 @@
+#Q1
+#num1=int(input("Enter the Number:"))
+#num2=int(input("Enter the Second Number:"))
+#sum=num1+num2
+#Diff=num1-num2
+#product=num1*num2
+#qoutient=num1/num2
+#print(sum,Diff,product,qoutient)
+
+
+#Q2
+#a=15
+#b=4
+#print("Floor Division is:",a//b)
+#print("The remainder is:",a%b)
+#print("The power is:",a**b)
+
+#Q3
+"""pen=int(input("Enter the number of pen:"))
+nb=int(input("Enter the no. of notebooks:"))
+total_cost=(15*pen)+(40*nb)
+print(total_cost)"""
+
+#Q4
+'''num1=int(input("enter the 1st num"))
+num2=int(input("enter the 2nd num"))
+num3=int(input("enter the 3rd num"))
+num4=int(input("enter the 4th num"))
+num5=int(input("enter the 5th num"))
+print("Average is",(num1+num2+num3+num4+num5)/5)'''
+
+#Q5
+'''a=int(input("Enter the number:"))
+print("Square is:",a**2)
+print("Cube is:",a**3)
+print("sqrt is:",a**0.5)'''
+
+# COMPARISON
+#Q1
+'''n1=int(input("Enter the Num1:"))
+n2=int(input("Enter the Num2:"))
+print(n1==n2)'''
+
+#Q2
+'''n1=int(input("Enter the Num1:"))
+n2=int(input("Enter the Num2:"))
+if(n1>n2):
+    print(n1)
+else:
+    print(n2)   ''' 
+    
+#Q3
+'''n1=int(input("Enter the num1:"))
+n2=int(input("Enter the num2:"))
+n3=int(input("Enter the num3:"))
+if(n1<n2)and(n2<n3):
+    print("n1 is smallest",n1)
+elif(n2<n3)and(n3<n1)    :
+    print("n2 is smallest",n2)
+else:
+    print("n3 is smallest",n3)    '''
+
+#Q4
+'''marks=int(input("Enter the marks:"))
+if(marks>=40):
+    print("Pass")
+else:
+    print("Fail")'''
+
+#Q5
+'''age=int(input("Enter the Age:"))
+if(age>=18):
+    print("Eligble")        
+else:
+    print("Not Eligble")    '''
+
+#Q6   
